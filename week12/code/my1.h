cd7bd891ee2987f50854ae7abc47487961a071ff
kitesugar <haohao7859415@163.com>
2019-12-05 06:14:38 -0800

    week11

---
+++ week12/code/my1.h
@@ -0,0 +1,10 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+#include<string.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<signal.h>
+#include<time.h>
